2445cda10d482b0dfe95acc816bb9ec9bb322479
Akshay anand <akshay1622004@gmail.com>
2024-07-01 18:55:45 +0530

    final touch for contents of frontend

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 function Navbar() {
+
+  
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" style={{height:"10vh"}}>
       <div className="container-fluid">
