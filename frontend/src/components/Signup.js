bf6c4300791c4a4ae294ae300cb3868220080a5d
Akshay anand <akshay1622004@gmail.com>
2024-07-04 20:35:55 +0530

    local storage update in signup

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,21 +1,90 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 function Signup() {
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    password: "",
+    role: "",
+  });
+
+  const [users, setUsers] = useState(() => {
+    const savedUsers = localStorage.getItem("users");
+    return savedUsers ? JSON.parse(savedUsers) : [];
+  });
+  useEffect(() => {
+    console.log(users);
+    localStorage.setItem("users", JSON.stringify(users));
+  }, [users]);
+
+  //const [role, setRole] = useState("")
+  const handleChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUsers((prevUsers) => [...prevUsers, user]);
+  };
+
   return (
-    <div style={{transform:"translateY(50px)"}}>
+    <div style={{ transform: "translateY(50px)" }}>
       <div className="form-container2">
-      <div className="form-box2">
-        <h2>Signup</h2>
-        <form>
-          <input type="text" placeholder="Username" className="input-field" required />
-          <input type="email" placeholder="Email" className="input-field" required />
-          <input type="password" placeholder="Password" className="input-field" required />
-          <button type="submit" className="submit-button">Signup</button>
-        </form>
+        <div className="form-box2">
+          <h2>Signup</h2>
+          <form>
+            <input
+              onChange={handleChange}
+              name="username"
+              value={user.username}
+              type="text"
+              placeholder="Username"
+              className="input-field"
+              required
+            />
+            <input
+              onChange={handleChange}
+              name="email"
+              value={user.email}
+              type="email"
+              placeholder="Email"
+              className="input-field"
+              required
+            />
+            <input
+              onChange={handleChange}
+              name="password"
+              value={user.password}
+              type="password"
+              placeholder="Password"
+              className="input-field"
+              required
+            />
+            <select
+              name="role"
+              id="role"
+              className="input-field"
+              required
+              onChange={handleChange}
+            >
+              <option value="">Select Role</option>
+              <option value="student">Student</option>
+              <option value="organization">Organization</option>
+            </select>
+            <button
+              type="submit"
+              className="submit-button"
+              onClick={handleSubmit}
+            >
+              Signup
+            </button>
+          </form>
+        </div>
       </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
